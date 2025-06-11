f0e73f6bebcd8e6ec02d430e37c273caf2bbef87
PrimeXploit <forgivetomyct@gmail.com>
2025-06-11 22:29:38 +0700

    Update Main_Script.lua

--- Main_Script.lua
+++ Main_Script.lua
@@ -4,5 +4,4 @@ _G.place = {
     ["126884695634066"] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/WhatTheSavage/Lua/refs/heads/main/Grow%20A%20Garden/Update_Logs.lua'))()"
 }
 
-print(_G.MainKey)
 loadstring(game:HttpGet('https://raw.githubusercontent.com/WhatTheSavage/Lua/refs/heads/main/gg.lua'))()
